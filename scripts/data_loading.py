36fe3914fa2696ef4a9fe45b213aae27f345ba9d
Wiktor Lazarski <wjlazarski@gmail.com>
2021-03-15 17:48:55 +0100

    update dataset interface

--- scripts/data_loading.py
+++ scripts/data_loading.py
@@ -2,4 +2,11 @@ import torch
 
 
 class SherlockDataset(torch.utils.data.Dataset):
-    pass
+    def __init__(self, story_window):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, idx):
+        pass
