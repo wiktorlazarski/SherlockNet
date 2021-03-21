bfa860716ee8a63e6e9ddc0f2b3aaded49bd053a
Wiktor Lazarski <wjlazarski@gmail.com>
2021-03-21 13:33:45 +0100

    model class ready for training

---
+++ scripts/model.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn as nn
+
+class SherlockLanguageModel(nn.Module):
+    def __init__(self, num_embeddings, embedding_dim, hidden_dim):
+        super().__init__()
+
+        self.token_embedding = nn.Embedding(
+            num_embeddings=num_embeddings,
+            embedding_dim=embedding_dim)
+
+        self.bi_gru = nn.GRU(
+            input_size=embedding_dim,
+            hidden_size=hidden_dim,
+            bidirectional=True,
+            batch_first=True
+        )
+
+        self.out_layer = nn.Linear(
+            in_features=2*hidden_dim,
+            out_features=num_embeddings
+        )
+
+    def forward(self, data_batch):
+        embeddings = self.token_embedding(data_batch)
+
+        _, out = self.bi_gru(embeddings)
+        out = torch.cat((out[0], out[1]), dim=1)
+
+        out = self.out_layer(out)
+
+        return out
+
+    def sample_story(self, starting_passage):
+        self.eval()
+
+        with torch.no_grad():
+            pass
+
+        return
