c207dda7c38271a16d7a11a2448229b0f016e02c
Wiktor Lazarski <wjlazarski@gmail.com>
2021-03-21 12:55:51 +0100

    SherlockDataset class impl

--- scripts/data_loading.py
+++ scripts/data_loading.py
@@ -1,12 +1,33 @@
+import json
+
 import torch
 
 
 class SherlockDataset(torch.utils.data.Dataset):
-    def __init__(self, story_window):
-        pass
+    def __init__(self, dataset_path, story_window, token_json):
+        self.story_window = story_window
+
+        with open(dataset_path, "r") as dset_file:
+            self.dataset = dset_file.read().split()
+
+        with open(token_json, "r") as json_file:
+            self.tokens = json.load(json_file)
+            self.tokens = dict(zip(self.tokens, range(len(self.tokens))))
 
     def __len__(self):
-        pass
+        return len(self.dataset) - self.story_window - 1
 
     def __getitem__(self, idx):
-        pass
+        story = self.dataset[idx : idx + self.story_window].copy()
+        target = self.dataset[idx + self.story_window]
+
+        story = self._one_hot_encoding(story)
+        target = self._one_hot_encoding(target)
+
+        return torch.tensor(story), torch.tensor(target)
+
+    def _one_hot_encoding(self, tokens):
+        if isinstance(tokens, str):
+            tokens = [tokens]
+
+        return [self.tokens[story_token] for story_token in tokens]
--- scripts/data_preprocessing.py
+++ scripts/data_preprocessing.py
@@ -14,7 +14,7 @@ class BytePairEncoding:
         tokens, vocab_encoded = cls._create_tokens(corpus, num_merges)
 
         def token_len(token):
-            return len(token[:-4]) + 1 if token.endswith('</w>') else len(token)
+            return len(token[:-4]) + 1 if token.endswith("</w>") else len(token)
 
         tokens = sorted(tokens.keys(), key=token_len, reverse=True)
 
@@ -22,10 +22,10 @@ class BytePairEncoding:
 
     @classmethod
     def load(cls, token_json, vocab_json):
-        with open(token_json, 'r') as token_json:
+        with open(token_json, "r") as token_json:
             tokens = json.load(token_json)
 
-        with open(vocab_json, 'r') as vocab_json:
+        with open(vocab_json, "r") as vocab_json:
             vocab = json.load(vocab_json)
 
         return cls(tokens, vocab)
@@ -51,7 +51,7 @@ class BytePairEncoding:
             for token in word_tokens:
                 tokens[token] += freq
 
-            word = ''.join(word_tokens).replace('</w>', '')
+            word = "".join(word_tokens).replace("</w>", "")
             vocab_encoded[word] = word_tokens
 
         return tokens, vocab_encoded
@@ -78,53 +78,63 @@ class BytePairEncoding:
     @classmethod
     def _merge_vocab(self, pair, v_in):
         v_out = {}
-        bigram = re.escape(' '.join(pair))
-        p = re.compile(r'(?<!\S)' + bigram + r'(?!\S)')
+        bigram = re.escape(" ".join(pair))
+        p = re.compile(r"(?<!\S)" + bigram + r"(?!\S)")
 
         for word in v_in:
-            w_out = p.sub(''.join(pair), word)
+            w_out = p.sub("".join(pair), word)
             v_out[w_out] = v_in[word]
 
         return v_out
 
-    def encode(self, word, unknown_token='</u>'):
+    def encode(self, word, unknown_token="</u>"):
         if word in self.vocab.keys():
             return self.vocab[word]
 
-        word += '</w>'
+        word += "</w>"
         return self._encode(word, self.tokens, unknown_token)
 
     def _encode(self, word, sorted_tokens, unknown_token):
-        if word == '':
+        if word == "":
             return []
 
         word_tokens = []
         for i in range(len(sorted_tokens)):
             token = sorted_tokens[i]
-            token_reg = re.escape(token.replace('.', '[.]'))
+            token_reg = re.escape(token.replace(".", "[.]"))
 
-            matched_positions = [(m.start(0), m.end(0)) for m in re.finditer(token_reg, word)]
+            matched_positions = [
+                (m.start(0), m.end(0)) for m in re.finditer(token_reg, word)
+            ]
             if len(matched_positions) == 0:
                 continue
 
-            substring_end_positions = [matched_position[0] for matched_position in matched_positions]
+            substring_end_positions = [
+                matched_position[0] for matched_position in matched_positions
+            ]
 
             substring_start_position = 0
             for substring_end_position in substring_end_positions:
-                substring = word[substring_start_position:substring_end_position]
-                word_tokens += self._encode(substring, sorted_tokens[i+1:], unknown_token)
+                substring = word[
+                    substring_start_position:substring_end_position
+                ]
+                word_tokens += self._encode(
+                    substring, sorted_tokens[i + 1 :], unknown_token
+                )
                 word_tokens += [token]
                 substring_start_position = substring_end_position + len(token)
 
             remaining_substring = word[substring_start_position:]
-            word_tokens += self._encode(remaining_substring, sorted_tokens[i+1:], unknown_token)
+            word_tokens += self._encode(
+                remaining_substring, sorted_tokens[i + 1 :], unknown_token
+            )
             break
 
-        return word_tokens if sorted_tokens[i+1:] != [] else [unknown_token]
+        return word_tokens if sorted_tokens[i + 1 :] != [] else [unknown_token]
 
     def decode(self, encoded_text):
-        text = ''.join(encoded_text)
-        text = text.replace('</w>', '')
+        text = "".join(encoded_text)
+        text = text.replace("</w>", "")
 
         return text
 
@@ -134,20 +144,20 @@ def merge_corpora(data_dir):
     for book in os.listdir(data_dir):
         book_path = os.path.join(data_dir, book)
 
-        with open(book_path, 'r') as file:
+        with open(book_path, "r") as file:
             corpora.append(file.read())
 
-    return '\n'.join(corpora)
+    return "\n".join(corpora)
 
 
 def create_jsons(corpus, num_iters, token_json, vocab_json):
     encoder = BytePairEncoding.create(corpus, num_iters)
 
-    with open(token_json, 'w') as json:
-        json.dump(encoder.tokens, json)
+    with open(token_json, "w") as json_file:
+        json.dump(encoder.tokens, json_file)
 
-    with open(vocab_json, 'w') as json:
-        json.dump(encoder.vocab, json)
+    with open(vocab_json, "w") as json_file:
+        json.dump(encoder.vocab, json_file)
 
 
 def create_encoded_corpus_file(corpus, out_path, token_json, vocab_json):
@@ -156,19 +166,19 @@ def create_encoded_corpus_file(corpus, out_path, token_json, vocab_json):
     encoder = BytePairEncoding.load(token_json, vocab_json)
 
     corpus = [encoder.encode(word) for word in corpus]
-    corpus = [' '.join(encoded_word) for encoded_word in corpus]
-    corpus = ' '.join(corpus)
+    corpus = [" ".join(encoded_word) for encoded_word in corpus]
+    corpus = " ".join(corpus)
 
-    with open(out_path, 'w') as out_file:
+    with open(out_path, "w") as out_file:
         out_file.write(corpus)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     corpus = merge_corpora("./data/processed/sherlock")
 
     create_encoded_corpus_file(
         corpus,
-        './data/encoded_corpus.txt',
-        './assets/tokens_1k.json',
-        './assets/vocab_1k.json'
+        "./data/encoded_corpus.txt",
+        "./assets/tokens_1k.json",
+        "./assets/vocab_1k.json",
     )
