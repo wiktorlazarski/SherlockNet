eef42b91a19bd8dace7bb4fdfb975ab7a6f077e9
Wiktor Lazarski <wjlazarski@gmail.com>
2021-03-25 22:08:57 +0100

    bug fix in story sampling

--- scripts/model.py
+++ scripts/model.py
@@ -39,10 +39,11 @@ class SherlockLanguageModel(nn.Module):
 
         with torch.no_grad():
             generated_story = passage.copy()
-            if len(passage) > story_window:
-                passage = passage[:-story_window]
+            if len(generated_story) > story_window:
+                passage = generated_story[-story_window:]
 
             for _ in range(length):
+                print(len(passage))
                 passage = torch.tensor(passage).unsqueeze(dim=0)
 
                 preds = self.forward(passage)
@@ -53,11 +54,52 @@ class SherlockLanguageModel(nn.Module):
                 new_token = y_pred.item()
 
                 generated_story.append(new_token)
-                passage = generated_story.copy()
 
                 if len(generated_story) > story_window:
-                    passage = passage[1:]
+                    passage = generated_story[-story_window:]
+                else:
+                    passage = generated_story
 
         return generated_story
 
-        return
+
+if __name__ == "__main__":
+    device = torch.device('cpu')
+    state_dict = torch.load(
+        './models/sw16_epoch25_LM_GPU_e512_h1028.pth',
+        map_location=device
+    )
+
+    import scripts.data_preprocessing as dp
+    bpe = dp.BytePairEncoding.load("./assets/tokens_1k.json", "./assets/vocab_1k.json")
+
+    sherlock_lm = SherlockLanguageModel(
+        num_embeddings=len(bpe.tokens),
+        embedding_dim=512,
+        hidden_dim=1028
+    )
+    sherlock_lm.load_state_dict(state_dict["LM"])
+    print("Model loaded successfully")
+
+    init_passage = "Wiktor was a Sherlock friend who helped him when"
+
+    encoded_passage = [bpe.encode(word) for word in init_passage.split()]
+    encoded_passage = [item for sublist in encoded_passage for item in sublist]
+
+    tokens = dict(zip(bpe.tokens, range(len(bpe.tokens))))
+    encoded_passage = [tokens[story_token] for story_token in encoded_passage]
+
+
+    temperature = 0.005
+    story = sherlock_lm.sample_story(encoded_passage, temperature, 100)
+    print("Sampling finished")
+
+    tokens = dict(zip(range(len(bpe.tokens)), bpe.tokens))
+    story = [tokens[token_index] for token_index in story]
+
+    story = ' '.join(story)
+    story = story.split("</w>")
+    story = [word.replace(" ", "") for word in story]
+
+    story = ' '.join(story)
+    print(story)
